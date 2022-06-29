1aeb4be4d567e965cefc33ac3853003a088d5699
Mobina Shahbazi <mbn.shahbazi@gmail.com>
2022-06-29 12:42:55 +0430

    feature add comment

--- controller/controller.java
+++ controller/controller.java
@@ -163,6 +163,7 @@ public class controller {
     }
     private String addComment(HashMap<String,String> data){
         try {
+            database.getInstance().getTable(data.get("title")).insert(data);
             return "massage successfully saved\u0000";
         }catch (Exception e){return "somethings goes wrong\u0000";}
     }
